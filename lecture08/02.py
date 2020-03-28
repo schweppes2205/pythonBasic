bee778aba65d5e35510441abf49e098085821074
user01 <schweppes2205@gmail.com>
2020-03-28 23:46:27 +0300

    lecture 08. task 02

--- lecture08/02.py
+++ lecture08/02.py
@@ -6,4 +6,32 @@
 """
 
 
-class My_Exception(Exception):
+class MyException(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f"Custom message is: {self.message}"
+        else:
+            return "No custom message. Just exception raise"
+
+
+while True:
+    while True:
+        try:
+            x, y = (input("Please enter 2 digits separating them with space for division. ")).split()
+            x, y = int(x), int(y)
+            break
+        except ValueError:
+            print("Please enter 2 digits only")
+            continue
+    try:
+        if not y:
+            raise MyException("it's zero division error. Please enter non-zero second value.")
+        print(f"{x}/{y} will be {(x / y):.2f}")
+    except MyException as e:
+        print(e)
